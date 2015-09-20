ecb9c9fb780b74b3c28adf47ec4f5c2729421938
AlineUzita <aline.uzita@hotmail.com>
2015-09-20 18:34:19 -0300

    Foi criado os atributos com get e set de cada um deles da classe Editora

---
+++ ProjetoLivraria/src/br/ifsp/livraria/pojo/Editora.java
@@ -0,0 +1,34 @@
+package br.ifsp.livraria.pojo;
+
+public class Editora {
+
+	private String nomeEditora;
+	private String endereco;
+	private String telefone;
+	private String cnpj;
+	
+	public String getNomeEditora() {
+		return nomeEditora;
+	}
+	public void setNomeEditora(String nomeEditora) {
+		this.nomeEditora = nomeEditora;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+}
