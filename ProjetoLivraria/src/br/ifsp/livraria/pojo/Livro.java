7287e206e14f1246229c16d6ebd8a26d1d1dcff4
AlineUzita <aline.uzita@hotmail.com>
2015-10-01 02:39:59 -0300

    Foi modificado a Classe Livro. Foi tirado o construtor para manter o padrão das demais classes e acrescentado os SETTERS, porque só tinha os GETTERS.

--- ProjetoLivraria/src/br/ifsp/livraria/pojo/Livro.java
+++ ProjetoLivraria/src/br/ifsp/livraria/pojo/Livro.java
@@ -5,88 +5,90 @@ package br.ifsp.livraria.pojo;
  
  public class Livro {
  
- 	//Todos os usuï¿½rios
+ 	//Todos os usuários
  	private int isbn;
- 	private String titulo;
+	private String titulo;
  	private String dataDePublicacao; 
  	private Editora editora; 
- 	private String categoria; //Originalmente String
+ 	private String categoria; 
  	private double precoDeVenda;
  	private String resumo;
- 	private String indice; //capï¿½tulos todos numerados
+ 	private String indice; 
  	
- 	private final List<Autor> autores = new ArrayList<Autor>(); 
-
- 	//Somente usuï¿½rios internos
+ 	//Somente usuários internos
  	private double precoDeCusto;
  	private double margemDeLucro;
  	private int quantidadeEmEstoque;
- 	
- 	//Construtor - todos os atributos exceto autores 
- 	public Livro(int isbn, String titulo, String dataDePublicacao,
- 			Editora editora, String categoria, double precoDeVenda,
- 			String resumo, String indice, double precoDeCusto,
- 			double margemDeLucro, int quantidadeEmEstoque) {
- 		
- 		this.isbn = isbn;
- 		this.titulo = titulo;
- 		this.dataDePublicacao = dataDePublicacao;
- 		this.editora = editora;
- 		this.categoria = categoria;
- 		this.precoDeVenda = precoDeVenda;
- 		this.resumo = resumo;
- 		this.indice = indice;
- 		this.precoDeCusto = precoDeCusto;
- 		this.margemDeLucro = margemDeLucro;
- 		this.quantidadeEmEstoque = quantidadeEmEstoque;
- 		
- 	}
- 
- 	public Integer getIsbn() {
- 		return isbn;
- 	}
- 
- 	public String getTitulo() {
- 		return titulo;
- 	}
- 
- 	public String getDataDePublicacao() {
- 		return dataDePublicacao;
- 	}
- 
- 	public Editora getEditora() {
- 		return editora;
- 	}
- 
- 	public String getCategoria() {
- 		return categoria;
- 	}
- 
- 	public double getPrecoDeVenda() {
- 		return precoDeVenda;
- 	}
- 
- 	public String getResumo() {
- 		return resumo;
- 	}
- 
- 	public String getIndice() {
- 		return indice;
- 	}
- 
- 	public List<Autor> getAutores() {
- 		return autores;
- 	}
- 
- 	public double getPrecoDeCusto() {
- 		return precoDeCusto;
- 	}
- 
- 	public double getMargemDeLucro() {
- 		return margemDeLucro;
- 	}
- 	
- 	public int getQuantidadeEmEstoque() {
- 		return quantidadeEmEstoque;
-	}	
+
+ 	private final List<Autor> autores = new ArrayList<Autor>();
+
+	public int getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDataDePublicacao() {
+		return dataDePublicacao;
+	}
+	public void setDataDePublicacao(String dataDePublicacao) {
+		this.dataDePublicacao = dataDePublicacao;
+	}
+	public Editora getEditora() {
+		return editora;
+	}
+	public void setEditora(Editora editora) {
+		this.editora = editora;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public double getPrecoDeVenda() {
+		return precoDeVenda;
+	}
+	public void setPrecoDeVenda(double precoDeVenda) {
+		this.precoDeVenda = precoDeVenda;
+	}
+	public String getResumo() {
+		return resumo;
+	}
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+	public String getIndice() {
+		return indice;
+	}
+	public void setIndice(String indice) {
+		this.indice = indice;
+	}
+	public double getPrecoDeCusto() {
+		return precoDeCusto;
+	}
+	public void setPrecoDeCusto(double precoDeCusto) {
+		this.precoDeCusto = precoDeCusto;
+	}
+	public double getMargemDeLucro() {
+		return margemDeLucro;
+	}
+	public void setMargemDeLucro(double margemDeLucro) {
+		this.margemDeLucro = margemDeLucro;
+	}
+	public int getQuantidadeEmEstoque() {
+		return quantidadeEmEstoque;
+	}
+	public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+		this.quantidadeEmEstoque = quantidadeEmEstoque;
+	}
+	public List<Autor> getAutores() {
+		return autores;
+	}
 }
